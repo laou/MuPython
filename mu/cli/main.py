73bd2f0730d23ba0b2eea79ca25eb1b4682a3184
laou <laou_lucas@yahoo.fr>
2013-03-03 22:44:18 +0100

    Add --version to the argument parser.

--- mu/cli/main.py
+++ mu/cli/main.py
@@ -20,6 +20,17 @@ def main():
         action='version',
         version='mu %s' % __version__,
     )
+    
+    args = p.parse_args()
+    
+    try:
+        args.func(args, p)
+    except Exception as e:
+        print "An unexpected error has occurred, please consider sending the following traceback to the μPython GitHub issue tracker at https://github.com/laou/MuPython/issues"
+        print
+        exc_info = sys.exc_info()
+        raise exc_info[1], None, exc_info[2]    
+    
 
 if __name__ == '__main__':
     main()
